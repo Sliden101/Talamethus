c3ea8c87cf04bf08872f3882b1ef91443025f651
Sliden101 <chessplayz11@gmail.com>
2021-10-30 00:31:55 +0700

    kek

---
+++ commands/Minecraft/ip.js
@@ -0,0 +1,11 @@
+const Discord = require("discord.js")
+const config = require("./config.json")
+module.exports.run = async (bot, message, args) => {
+    message.channel.send({
+        embeds: [new MessageEmbed().setTitle("Here!").setColor("RED").setDescription(`IP: ${config.ip}ms`)]
+    });
+}
+module.exports.info = {
+    name: 'ip', // default = file name (without the extention)
+    description: "Shows the server's ip." // default is "None"
+}
