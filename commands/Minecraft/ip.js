e02438760db0f90b18ebf897288b794561871dfc
Sliden101 <chessplayz11@gmail.com>
2021-10-30 00:34:16 +0700

    lmao i forgor

--- commands/Minecraft/ip.js
+++ commands/Minecraft/ip.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js")
+const { MessageEmbed } = require("discord.js")
 const config = require("./config.json")
 module.exports.run = async (bot, message, args) => {
     message.channel.send({
