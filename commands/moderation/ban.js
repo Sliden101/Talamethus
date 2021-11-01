e84e04b0e3e3e13e2d83a88f6220e678c4eeb0fd
Sliden101 <chessplayz11@gmail.com>
2021-11-01 19:58:07 +0700

    should probably work right lmaooo

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,1 +1,20 @@
-fuck this shit ill do it later
+const { MessageEmbed } = require("discord.js")
+
+module.exports.run = async (bot, message,args) =>{
+    if (msg.member.hasPermission("BAN_MEMBERS")) {
+        if (msg.members.mentions.first()) {
+            try {
+                msg.members.mentions.first().ban();
+            } catch {
+                msg.reply("I do not have permissions to ban" + msg.members.mentions.first());
+            }
+        } else {
+            msg.reply("You do not have permissions to ban" + msg.members.mentions.first());
+        }
+    }
+}
+
+module.exports.info = {
+    name: 'ban', // default = file name (without the extention)
+    description: "Bans the mentioned user." // default is "None"
+}
