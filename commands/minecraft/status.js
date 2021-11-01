66a010b14ddc1cfe9462e27f3e245b374a87ab70
Sliden101 <chessplayz11@gmail.com>
2021-11-01 12:24:35 +0700

    yay another thing doe

---
+++ commands/minecraft/status.js
@@ -0,0 +1,42 @@
+const { MessageEmbed } = require("discord.js")
+const config = require(ROOT_PATH + "/config.json")
+const rp = require("request-promise");
+
+module.exports.run = async (bot, message, args) => {
+    const server =  { name: "ESASMC", queryURL: "https://api.minetools.eu/query/play.esasmc.com/25565", pingURL: "https://api.minetools.eu/ping/play.esasmc.com/25565" };
+//dont want to config this shit lmao im hardcoding 
+    let msg = await message.channel.send({embeds: [
+        new MessageEmbed()
+            .setTitle("Loading server status... This may take a few seconds.")
+    ]});
+    let fields = []
+
+        await rp(server.pingURL).then(content => {
+                const json = JSON.parse(content);
+                if (json.error) fields.push({
+                    name: "ESASMC Offline",
+                    value: "ESASMC Offline"
+                })
+                else {
+                    const playerCount = json.players.online;
+                    fields.push({
+                        name: `ESASMC: ${playerCount} player(s)`,
+                        value: `ESASMC: ${playerCount} player(s)`
+                    })
+                }
+        })
+        let embed = new MessageEmbed()
+            .setTitle("ESASMC Network Server Status")
+            .setTimestamp()
+            .setColor("RANDOM")
+            .addFields(fields)
+        msg.edit({embeds: [embed]});
+    }
+/**
+ * This is completely optional...
+ */
+
+module.exports.info = {
+    name: 'status', // default = file name (without the extention)
+    description: "Shows the status of the server." // default is "None"
+}
