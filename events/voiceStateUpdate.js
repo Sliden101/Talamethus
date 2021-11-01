40724c4f91588bf90f727d20384fddafcbc85cfe
Sliden101 <chessplayz11@gmail.com>
2021-11-01 08:34:33 +0700

    workie

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,22 +1,22 @@
-const bot
+let { bot } = require(ROOT_PATH + '/index');
 const { Collection } = require('discord.js')
-const config = require('../config.json')
+const config = require(ROOT_PATH + '/config.json');
 const voiceCollection = new Collection();
 
-
 bot.on("voiceStateUpdate", async (oldState, newState) => {
-    const user = await client.users.fetch(newState.id)
-    const member = newState.guild.member(user);
-
+    const user = await bot.users.fetch(newState.id);
+    const member = newState.guild.members.cache.get(newState.id);
+    
     if (!oldState.channel && newState.channel.id === config.vcchild) {
         const channel = await newState.guild.channels.create(user.tag, {
-            type: 'voice',
+            type: 'GUILD_VOICE',
             parent: newState.channel.parent,
         });
-        member.voice.setChannel(channel);
+        member.voice.setChannel(channel.id);
+       
         voiceCollection.set(user.id, channel.id);
     } else if(!newState.channel){
-        if (oldState.channel === voiceCollection.get(newstate.id)) 
+        if (oldState.channelId === voiceCollection.get(newState.id)) 
         return oldState.channel.delete()
     }
 })
